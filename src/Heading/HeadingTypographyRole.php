92a342f8f323506eb61044202f445dca2f45768b
Peverelli Andrea <ndrpeverelli@gmail.com>
2025-09-22 17:56:02 +0200

    quickfix

--- src/Heading/HeadingTypographyRole.php
+++ src/Heading/HeadingTypographyRole.php
@@ -7,14 +7,14 @@ use Phx\Core\TypographyRole;
 enum HeadingTypographyRole
 {
 	case DISPLAY;
-	case HEADING;
+	case HEADLINE;
 	case TITLE;
 
 	final public function getTypographyRole(): TypographyRole
 	{
 		return match($this) {
 			self::DISPLAY => TypographyRole::DISPLAY,
-			self::HEADING => TypographyRole::HEADING,
+			self::HEADLINE => TypographyRole::HEADLINE,
 			self::TITLE => TypographyRole::TITLE,
 		};
 	}
